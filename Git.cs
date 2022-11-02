beeeaf83e5a8d0ebb30d87fb07013efd05ba70d8
Hitesh Jha <93187649+Dark2003fire@users.noreply.github.com>
2022-11-02 19:43:53 +0530

    Csharp file

---
+++ Git.cs
@@ -0,0 +1,22 @@
+using System;
+using namespace std{
+
+
+
+	class Git{
+		
+		public static void Main(string[]args){
+				
+                        Console.WriteLine("Hello This is my first file in my Git repo");
+			Console.WriteLine("Hello I've added one more file to it");
+
+
+
+			
+			}
+
+
+	}
+
+
+}
